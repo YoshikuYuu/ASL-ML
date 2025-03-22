53e31119241e61b26f5305709414e30b150a5d90
CloudyKrypton <janetfu04@gmail.com>
2025-03-21 23:05:26 -0400

    basic template

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__, static_url_path='/static')
 
+
 @app.route('/')
 @app.route('/aslml')
-def aslml():
+def asml():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=81)
